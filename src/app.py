10e6660ad9aa5e1f0ba55b3f77029764cd867b4b
JuanDavid24 <david.juan90.jd@gmail.com>
2025-06-12 21:11:31 -0300

    fix: correct plot title and enable octaves map plotting

--- src/app.py
+++ src/app.py
@@ -8,4 +8,4 @@ mapSetScales = [mg.generateMap(scale=s, octaves=1) for s in scales]
 mapSetOctaves = [mg.generateMap(scale=200, octaves=o) for o in octaves]
 
 mp.plotMaps(mapSetScales, ('scale', scales))
-# mp.plotMaps(mapSetOctaves, ('octaves', octaves))
+mp.plotMaps(mapSetOctaves, ('octaves', octaves))
