f92ffd5185142a356a719ba65274fcb1b983ea9f
JuanDavid24 <david.juan90.jd@gmail.com>
2025-06-29 21:15:19 -0300

    test: add diamond-square test

--- tests/test_diamondSquare.py
+++ tests/test_diamondSquare.py
@@ -29,10 +29,6 @@ mapC = np.array([[0.5, 0.0, 0.1, 0.0, 0.9],
                  [0.0, 0.5, 0.0, 0.5, 0.0],
                  [0.5, 0.0, 0.2, 0.0, 0.7]])
 chunkSizeC = 3
-# pointsC = [(1, 0), (3, 0), (0, 1), (2, 1), (4, 1), (1, 2), (3, 2), (0, 3), (2, 3), (4, 3), (1, 4), (3, 4)]
-# neighborsValuesC = [[0.5, 0.3, 0.5], [0.3, 0.5, 0.5], [0.5, 0.1, 0.5], [0.5, 0.5, 0.2, 0.3], [0.5, 0.2, 0.5], 
-#                     [0.1, 0.2, 0.5, 0.5], [0.2, 0.2, 0.5, 0.5], [0.5, 0.9, 0.1], [0.5, 0.5, 0.4, 0.2], [0.5, 0.7, 0.2],
-#                      [0.9, 0.4, 0.5], [0.4, 0.7, 0.5]]
 
 # for first point (0, 1), second point (0, 3), third point (1, 0)
 pointsC = [
@@ -71,15 +67,26 @@ def test_getSquareNeighborsValues(point, neighborsValues):
     result = ds.getSquareNeighborsValues(mapC, x, y, chunkSizeC)
     assert sorted(result) == sorted(neighborsValues)
 
-# @pytest.mark.parametrize("map, x , y, chunkSize", [(mapC, xC, yC, chunkSizeC)])
-# def test_calculateSquareValue(map, x, y, chunkSize):
-#     newMap = ds.calculateSquareValue(map, x, y, chunkSize)
-#     assert newMap[(x, y)] != 0
-
 @pytest.mark.parametrize("map, points, chunkSize", [(mapC, pointsC, chunkSizeC)])
-def test_calculateSquareStep(map, points, chunkSize):
+def test_squareStep(map, points, chunkSize):
     newMap = ds.squareStep(map, chunkSize)
     for p in points:
         y, x = p
         assert newMap[(y, x)] != 0
 
+@pytest.mark.parametrize("n, size, roughness, seed", [(3, 9, 1, [0.2, 0.5, 0.1, 0.9]), (4, 17, 1, None), (5, 33, 1, None)])
+def test_diamondSquare(n, size, roughness, seed):
+    map = ds.diamondSquare(n, roughness, seed)
+    # check map size
+    assert map.shape[0] == size
+    
+    # check seed if given
+    if seed is not None:
+        corners = [map[0, 0], map[0, size-1], map[size-1, 0], map[size-1, size-1]]
+        assert corners == seed
+        
+    # check if matrix is completelly populated
+    for row in map:
+        for p in row:
+            assert p is not 0.0
+    
