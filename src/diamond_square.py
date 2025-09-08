fix: - diamond-square map generation now clips values in each step. Not post-normalization anymore, because can't make maps with shorter range. -feat: plot functions now accepts min/max values to adapt colormap

src/diamond_square.py
@@ -43,8 +43,8 @@ def diamond_step(map, chunk_size, rng, roughness=1):
                 map[y, x + chunk_size - 1] + 
                 map[y + chunk_size - 1, x + chunk_size - 1]
                 ) / 4
-            value = rng.uniform(-1,1) * roughness # random value scaled by roughness
-            map[y + half, x + half] = avg + value
+            value = rng.uniform(-0.5, 0.5) * roughness # random value scaled by roughness
+            map[y + half, x + half] = np.clip(avg + value, 0, 1)
     return map
 
 def square_step(map, chunk_size, rng, roughness=1):
@@ -59,8 +59,8 @@ def square_step(map, chunk_size, rng, roughness=1):
 def calculate_square_value(map, x, y, chunk_size, rng, roughness=1):
     neighbor_values = get_square_neighbor_values(map, x, y, chunk_size)
     avg = sum(neighbor_values) / len(neighbor_values)
-    value = rng.uniform(-1,1) * roughness # random value scaled by roughness
-    map[y, x] = avg + value
+    value = rng.uniform(-0.5, 0.5) * roughness # random value scaled by roughness
+    map[y, x] = np.clip(avg + value, 0, 1)
     return map
 
 def get_square_neighbor_values(map, x, y, chunk_size):

src/map_generator.py
@@ -4,7 +4,6 @@ from diamond_square import diamond_square as ds
 
 def perlin_map(shape = (1024, 1024), scale = 100, octaves = 1, persistence = 0.5, lacunarity = 2.0):
     map = np.zeros(shape)
-
     for i in range(shape[0]):
         for j in range(shape[1]):
             map[i][j] = noise.pnoise2(i/scale, 
@@ -17,7 +16,7 @@ def perlin_map(shape = (1024, 1024), scale = 100, octaves = 1, persistence = 0.5
 
 def diamond_square_map(n, roughness=1, seed=None, corners=None):
     map = ds(n, roughness, seed, corners)
-    return normalize_map(map)
+    return map
 
 def normalize_map(map):
     return (map - map.min()) / (map.max() - map.min())