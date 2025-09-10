de077f82678fa36ac172822093ee51ac90b96913
JuanDavid24 <david.juan90.jd@gmail.com>
2025-09-10 14:57:04 -0300

    feat: add leaf drawing and new plant plotting parameters
    -added leaf drawing in plant plotter
    -added plant plotting parameters: branch/leaf color, leaf width/length, animation control (on/off and speed).
    -added more L-systems examples.

--- src/l_systems.py
+++ src/l_systems.py
@@ -1,7 +1,7 @@
 import turtle as tr
 
-def sentence_generator(axiom, rules, iterations):
-    for i in range(iterations):
+def sentence_generator(axiom, rules, n):
+    for i in range(n):
         sentence = ""
         for symbol in axiom:
             if symbol in rules:
@@ -11,7 +11,7 @@ def sentence_generator(axiom, rules, iterations):
         axiom = sentence
     return sentence                
 
-def plant_plotter(sentence, step, angle):
+def plant_plotter(sentence, step, angle, speed=6, branch_color="white", leaf_color="green", leaf_width=1.0, leaf_length=1.0, animation=True):
     # setup
     screen = tr.Screen()
     window_w = screen.window_width()
@@ -19,9 +19,12 @@ def plant_plotter(sentence, step, angle):
     tr.left(90)
     tr.teleport(-window_w/2 + 10, -window_h/2 + 10)
     tr.screensize(bg="black")
-    tr.color("white")
+    tr.color(branch_color)
     tr.write(sentence, font=("Arial", 8, "normal"))
     tr.teleport(0, -window_h/2 + 30)
+    tr.tracer(animation)
+    tr.speed(speed)
+    tr.shapesize(leaf_width, leaf_length) # leaves size (width, size)
 
     turtle_stack = []  # save turtle states (position and heading) checkpoints in a stack
     for symbol in sentence:
@@ -38,5 +41,8 @@ def plant_plotter(sentence, step, angle):
                 (target_x, target_y), heading = turtle_stack.pop()
                 tr.teleport(target_x, target_y)
                 tr.seth(heading)
-                
+            case "L":
+                tr.color(leaf_color)
+                tr.stamp()
+                tr.color(branch_color)
     tr.done()
--- src/plants.py
+++ src/plants.py
@@ -7,11 +7,61 @@ l_sys_a = {
     "axiom": "F"
 }
 
-n = 2
-angle = 30
-stroke_len = 15
+l_sys_a2 = {
+    "ruleset": {
+        "F": "F[+FL]F[-FL]F"
+    },
+    "axiom": "F"
+}
 
-sentence_a = lsys.sentence_generator(l_sys_a["axiom"], l_sys_a["ruleset"], n)
+l_sys_b = {
+    "ruleset": {
+        "F": "F[+F]F[-F][F]"
+    },
+    "axiom": "F"
+}
 
-lsys.plant_plotter(sentence_a, stroke_len, angle)
+l_sys_b2 = {
+    "ruleset": {
+        "F": "F[+FL]F[-FL][FL]"
+    },
+    "axiom": "F"
+}
+
+l_sys_c = {
+    "ruleset": {
+        "F": "FF-[-F+F+F]+[+F-F-F]"
+    },
+    "axiom": "F"
+}
+
+l_sys_c2 = {
+    "ruleset": {
+        "F": "FF-[-F+F+FL]+[+F-F-FL]"
+    },
+    "axiom": "F"
+}
+
+color_fucsia = "#FF0080"
+color_corn = "#FBEC5D"
+color_olive = "#808000"
+color_darkcyan = "#18848e"
+
+# sentence_a = lsys.sentence_generator(l_sys_a["axiom"], l_sys_a["ruleset"], 3)
+# lsys.plant_plotter(sentence_a, step=30, angle=30)
+
+# sentence_a2 = lsys.sentence_generator(l_sys_a2["axiom"], l_sys_a2["ruleset"], 3)
+# lsys.plant_plotter(sentence_a2, step=30, angle=30, leaf_length=2)
+
+# sentence_b = lsys.sentence_generator(l_sys_b["axiom"], l_sys_b["ruleset"], 4)
+# lsys.plant_plotter(sentence_b, step=15, angle=20, speed="fastest")
+
+# sentence_b2 = lsys.sentence_generator(l_sys_b2["axiom"], l_sys_b2["ruleset"], 5)
+# lsys.plant_plotter(sentence_b2, step=12, angle=20, leaf_length=1, leaf_width=0.8, leaf_color=color_fucsia, branch_color=color_darkcyan, animation=False)
+
+# sentence_c = lsys.sentence_generator(l_sys_c["axiom"], l_sys_c["ruleset"], 4)
+# lsys.plant_plotter(sentence_c, step=15, angle=22.5, leaf_length=1, leaf_width=0.8, leaf_color=color_corn, animation=False)
+
+# sentence_c2 = lsys.sentence_generator(l_sys_c2["axiom"], l_sys_c2["ruleset"], 4)
+# lsys.plant_plotter(sentence_c2, step=15, angle=22.5, leaf_length=2, leaf_width=0.6, leaf_color=color_corn, branch_color=color_olive, animation=False)
 
