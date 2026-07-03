cfeb8df8c4606e2d758580608165625a26767d34
JuanDavid24 <david.juan90.jd@gmail.com>
2026-07-03 12:55:36 -0300

    Feat: add diamond-square terrain creation to API

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from terrain.terrain_generator import perlin_map
+from terrain.terrain_generator import perlin_map, diamond_square_map
 from utils.logger import format_terrain_data
 
 class Perlin_params(BaseModel): 
@@ -11,6 +11,13 @@ class Perlin_params(BaseModel):
   persistence: float | None = 0.5
   lacunarity: float | None = 2
   
+class Diamond_square_params(BaseModel): 
+  seed: int | None = None
+  n: int
+  initial_corners: list[float] | None = None
+  roughness: int | None = 1
+  wrap: bool | None = False
+  
 app = FastAPI()
 
 @app.get("/")
@@ -19,7 +26,7 @@ def read_root():
 
 @app.post("/terrain/perlin")
 async def create_terrain_perlin(args: Perlin_params):
-  terrain_args = args.model_dump()  # JSON to dict
+  terrain_args = args.model_dump()  # model to dict
   terrain_args["shape"] = (args.size, args.size)
   terrain_args.pop("size")  # not needed for perlin_map function
   
@@ -27,5 +34,20 @@ async def create_terrain_perlin(args: Perlin_params):
   terrain_args.update({"seed": seed}) # update seed if not given in req
   
   res_data = format_terrain_data(algorithm="perlin_noise", map=map, size=args.size, **terrain_args)
+
+  return res_data
+
+@app.post("/terrain/diamond-square")
+async def create_terrain_diamond_square(args: Diamond_square_params):
+  terrain_args = args.model_dump()  # model to dict
+  
+  map, seed, corners = diamond_square_map(**terrain_args, debug=True)
+  
+  # update dict
+  terrain_args.update({"seed": seed})  # update seed if not given in req
+  terrain_args.update({"initial_corners": corners}) # update initial_corners if not given in req
+  terrain_args.update({"size": 2**args.n + 1}) 
+  
+  res_data = format_terrain_data(algorithm="diamond_square", map=map, **terrain_args)
 
   return res_data
