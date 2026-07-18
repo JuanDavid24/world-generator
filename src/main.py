5f9a69ebd1a6bcae667e687302d7b2f82105b669
JuanDavid24 <david.juan90.jd@gmail.com>
2026-07-18 20:07:55 -0300

    Feat: Add CORS middleware

--- src/main.py
+++ src/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 from routers import terrain, vegetation
+from fastapi.middleware.cors import CORSMiddleware
   
 app = FastAPI()
+
+origins = ["http://localhost:5173"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(terrain.router)
 app.include_router(vegetation.router)
