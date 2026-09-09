19f9712886a73f14d7adf7f0243a551fc4032529
JuanDavid24 <31551453+JuanDavid24@users.noreply.github.com>
2026-09-08 21:08:21 -0300

    Misc: changed param names in normalize_map

--- src/engine/utils/normalize.py
+++ src/engine/utils/normalize.py
@@ -1,3 +1,3 @@
-def normalize_map(map, min, max):
+def normalize_map(map, min_val, max_val):
     """ Normalizes a map to a given range [min, max]"""
-    return (map - map.min()) / (map.max() - map.min()) * (max - min) + min
+    return (map - map.min()) / (map.max() - map.min()) * (max_val - min_val) + min_val
