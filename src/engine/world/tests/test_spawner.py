5b01bc052d99e668a2c8ef84b4e03df06b20ccc5
JuanDavid24 <david.juan90.jd@gmail.com>
2026-09-18 00:36:44 -0300

    Tests: add tests for base axes and jitterer positions. Improved test data generation with fixtures

--- src/engine/world/tests/test_spawner.py
+++ src/engine/world/tests/test_spawner.py
@@ -5,41 +5,73 @@ from engine.terrain import diamond_square as ds
 from engine.terrain import perlin_noise as pn
 from engine.world.spawner import Spawner, Vegetation_params
 
-# data
-map_empty_5x5 = np.zeros((5, 5))
-corners_a = [0.5, 0.9, 0.6, 0.7]
+# --- dataset ---
+TERRAIN_DATA = [            # mock terrain of zeros
+    np.zeros((16, 16)),     # pair
+    np.zeros((15, 15))      # odd
+]
+DISTANCE_DATA = [7, 6, 4]
 
-# perlin noise dataset
-TERRAIN_SIZE_PN = 64
-TERRAIN_SCALE_PN = 100
-TERRAIN_SEED_PN = 123
+# --- fixtures ---
+@pytest.fixture(params=TERRAIN_DATA)
+def multi_terrain(request):
+    return request.param
 
-DISTANCE_PN = 8
-VEGETATION_COVER_PN = 0.7
-VEGETATION_PN = Vegetation_params(DISTANCE_PN, VEGETATION_COVER_PN)
-SPAWN_SEED_PN = 321
-NOISE_MAP_SHAPE_PN = (8, 8)
+@pytest.fixture(params=DISTANCE_DATA)
+def multi_vegetation(request):
+    return Vegetation_params(distance=request.param, cover=0.7)
 
-terrain_pn_64_a, _ = pn.generate_perlin_map(shape=(TERRAIN_SIZE_PN, TERRAIN_SIZE_PN), scale=TERRAIN_SCALE_PN, seed=TERRAIN_SEED_PN)
-spawner_pn = Spawner(terrain_pn_64_a, VEGETATION_PN, SPAWN_SEED_PN)
+@pytest.fixture
+def dynamic_spawner(multi_terrain, multi_vegetation):
+    return Spawner(terrain=multi_terrain, vegetation=multi_vegetation, seed=123)
 
-# diamond-square dataset
-TERRAIN_N = 6
-TERRAIN_SIZE_DS = 2**TERRAIN_N - 1
-TERRAIN_SCALE_DS = 100
-TERRAIN_SEED_DS = 456
+# --- tests ---
+def test_noise_map_shape(dynamic_spawner):
+    terrain_size = dynamic_spawner.terrain_size
+    distance = dynamic_spawner.vegetation.distance
+    
+    # calculaate expected values
+    expected_noise_size = terrain_size // distance
+    expected_noise_shape = (expected_noise_size, expected_noise_size)
+    
+    assert dynamic_spawner.noise_map.shape == expected_noise_shape
 
-DISTANCE_DS = 8
-VEGETATION_COVER_DS = 0.7
-VEGETATION_DS = Vegetation_params(DISTANCE_DS, VEGETATION_COVER_DS)
-SPAWN_SEED_DS = 321
-NOISE_MAP_SHAPE_DS = (7, 7)
-
-terrain_ds_64_a, _ = pn.generate_perlin_map(shape=(TERRAIN_SIZE_DS, TERRAIN_SIZE_DS), scale=TERRAIN_SCALE_DS, seed=TERRAIN_SEED_DS)
-spawner_ds = Spawner(terrain_ds_64_a, VEGETATION_DS, SPAWN_SEED_DS)
-
-# check noise map
-@pytest.mark.parametrize("spawner, noise_map_shape", [(spawner_pn, NOISE_MAP_SHAPE_PN), (spawner_ds, NOISE_MAP_SHAPE_DS)])
-def test_noise_map(spawner, noise_map_shape):
-    output_noise_map_shape = spawner.noise_map.shape
-    assert np.array_equal(output_noise_map_shape, noise_map_shape)
+def test_base_axes(dynamic_spawner):
+    # generate base axes
+    base_x, base_y = dynamic_spawner.create_base_axes()
+    
+    # calculate expected base axes
+    distance = dynamic_spawner.vegetation.distance
+    terrain_size = dynamic_spawner.terrain_size
+    
+    cell_count = terrain_size // distance
+    cell_offset = distance / 2.0
+    
+    expected_centers = [cell_offset + i * distance for i in range(cell_count)]   # list of cell centers 
+    expected_base_x = np.array(expected_centers, dtype=np.float64)[None, :]      # transformed to row vector and numpy array  
+    expected_base_y = expected_base_x.T
+    
+    assert np.allclose(base_x, expected_base_x)
+    assert np.allclose(base_y, expected_base_y)
+    
+def test_jittered_positions(dynamic_spawner):
+    # generate jittered x and y matrix
+    jittered_positions_x, jittered_positions_y = dynamic_spawner.create_jittered_positions()
+    
+    # assert same shape as noise map
+    noise_map_shape = dynamic_spawner.noise_map.shape
+    assert(noise_map_shape == jittered_positions_x.shape)
+    assert(noise_map_shape == jittered_positions_y.shape)
+    
+    # check positions be inside terrain
+    terrain_size = dynamic_spawner.terrain_size
+    positions_x_in_bound = np.all((0 < jittered_positions_x) & 
+                                  (jittered_positions_x < terrain_size))
+    positions_y_in_bound = np.all((0 < jittered_positions_y) & 
+                                      (jittered_positions_y < terrain_size))
+    assert positions_x_in_bound
+    assert positions_y_in_bound
+    
+    
+    
+    
